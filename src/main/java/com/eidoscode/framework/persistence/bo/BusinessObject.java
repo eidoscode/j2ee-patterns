297f962dd5f65f8c519cf53bc2d45bcee414344e
Endrigo Antonini <antonini@endrigo.com.br>
2013-07-28 21:34:44 -0300

    #4 - Added the methods to remove an entity by its key

--- src/main/java/com/eidoscode/framework/persistence/bo/BusinessObject.java
+++ src/main/java/com/eidoscode/framework/persistence/bo/BusinessObject.java
@@ -15,86 +15,107 @@ import com.eidoscode.framework.persistence.model.Model;
  * @version 1.0
  * @since 1.0
  * @param <Key>
- *            Type of the Key of the Entity (Must implements the interface
- *            {@link Serializable}).
+ *          Type of the Key of the Entity (Must implements the interface
+ *          {@link Serializable}).
  * @param <Bean>
- *            The Bean (Must implements the interface {@link Model} ).
+ *          The Bean (Must implements the interface {@link Model} ).
  * @param <DAO>
- *            The Data Access Object Interface (Must implements the interface
- *            {@link DataAccessObject}).
+ *          The Data Access Object Interface (Must implements the interface
+ *          {@link DataAccessObject}).
  */
 public interface BusinessObject<Key extends Serializable, Bean extends Model<Key>, DAO extends DataAccessObject<Key, Bean>> {
 
-	/**
-	 * Saves the desired entity. It means it will persist a new entity or merge
-	 * an existent entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            desired entity.
-	 * @return entity stored
-	 */
-	Bean save(Bean bean);
+  /**
+   * Saves the desired entity. It means it will persist a new entity or merge an
+   * existent entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          desired entity.
+   * @return entity stored
+   */
+  Bean save(Bean bean);
 
-	/**
-	 * Saves the desired entity. It means it will persist a new entity or merge
-	 * an existent entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            desired entity.
-	 * @param flush
-	 *            If <code>true</code> the method {@link EntityManager#flush()}
-	 *            will be called.
-	 * @return entity stored
-	 */
-	Bean save(Bean bean, boolean flush);
+  /**
+   * Saves the desired entity. It means it will persist a new entity or merge an
+   * existent entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          desired entity.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   * @return entity stored
+   */
+  Bean save(Bean bean, boolean flush);
 
-	/**
-	 * Removes a desired entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Desired entity.
-	 */
-	void remove(Bean bean);
+  /**
+   * Removes a desired entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Desired entity.
+   */
+  void remove(Bean bean);
 
-	/**
-	 * Removes a desired entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Desired entity.
-	 * @param flush
-	 *            If <code>true</code> the method {@link EntityManager#flush()}
-	 *            will be called.
-	 */
-	void remove(Bean bean, boolean flush);
+  /**
+   * Removes a desired entity by an id.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Desired entity.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   */
+  void remove(Bean bean, boolean flush);
 
-	/**
-	 * Brings all the entities.
-	 * 
-	 * @since 1.0
-	 * @return {@link List} with the entities.
-	 */
-	List<Bean> findAll();
+  /**
+   * Removes a desired entity.
+   * 
+   * @since 1.0
+   * @param key
+   *          Entity key.
+   */
+  void removeById(Key key);
 
-	/**
-	 * Count all the entities.
-	 * 
-	 * @since 1.0
-	 * @return the amount of entities.
-	 */
-	Long countAll();
+  /**
+   * Removes a desired entity by an id.
+   * 
+   * @since 1.0
+   * @param key
+   *          Entity key.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   */
+  void removeById(Key key, boolean flush);
 
-	/**
-	 * Brings an entity by its key.
-	 * 
-	 * @since 1.0
-	 * @param id
-	 *            Entity key.
-	 * @return Entity.
-	 */
-	Bean findByKey(Key id);
+  /**
+   * Brings all the entities.
+   * 
+   * @since 1.0
+   * @return {@link List} with the entities.
+   */
+  List<Bean> findAll();
+
+  /**
+   * Count all the entities.
+   * 
+   * @since 1.0
+   * @return the amount of entities.
+   */
+  Long countAll();
+
+  /**
+   * Brings an entity by its key.
+   * 
+   * @since 1.0
+   * @param id
+   *          Entity key.
+   * @return Entity.
+   */
+  Bean findByKey(Key id);
 
 }
--- src/main/java/com/eidoscode/framework/persistence/bo/impl/BusinessObjectImpl.java
+++ src/main/java/com/eidoscode/framework/persistence/bo/impl/BusinessObjectImpl.java
@@ -22,194 +22,222 @@ import com.eidoscode.framework.persistence.model.Model;
  * @since 1.0
  * 
  * @param <Key>
- *            Type of the Key of the Entity (Must implements the interface
- *            {@link Serializable}).
+ *          Type of the Key of the Entity (Must implements the interface
+ *          {@link Serializable}).
  * @param <Bean>
- *            The Bean (Must implements the interface {@link Model} ).
+ *          The Bean (Must implements the interface {@link Model} ).
  * @param <DAO>
- *            The Data Access Object Interface (Must implements the interface
- *            {@link DataAccessObject}).
+ *          The Data Access Object Interface (Must implements the interface
+ *          {@link DataAccessObject}).
  */
 public abstract class BusinessObjectImpl<Key extends Serializable, Bean extends Model<Key>, DAO extends DataAccessObject<Key, Bean>>
-		implements BusinessObject<Key, Bean, DAO> {
+    implements BusinessObject<Key, Bean, DAO> {
 
-	private final Logger logger;
+  private final Logger logger;
 
-	/**
-	 * Main constructor.
-	 * 
-	 * @since 1.0
-	 */
-	public BusinessObjectImpl() {
-		logger = Logger.getLogger(getClass());
-	}
+  /**
+   * Main constructor.
+   * 
+   * @since 1.0
+   */
+  public BusinessObjectImpl() {
+    logger = Logger.getLogger(getClass());
+  }
 
-	public Logger getLogger() {
-		return logger;
-	}
+  public Logger getLogger() {
+    return logger;
+  }
 
-	/**
-	 * The purpose of this method is to bring the DAO instance.
-	 * 
-	 * @since 1.0
-	 * @return DAO instance.
-	 */
-	protected abstract DAO getDAO();
+  /**
+   * The purpose of this method is to bring the DAO instance.
+   * 
+   * @since 1.0
+   * @return DAO instance.
+   */
+  protected abstract DAO getDAO();
 
-	/**
-	 * Brings all the entities.
-	 * 
-	 * @since 1.0
-	 * @return {@link List} with the entities.
-	 */
-	@Override
-	public List<Bean> findAll() {
-		return getDAO().findAll();
-	}
+  /**
+   * Brings all the entities.
+   * 
+   * @since 1.0
+   * @return {@link List} with the entities.
+   */
+  @Override
+  public List<Bean> findAll() {
+    return getDAO().findAll();
+  }
 
-	/**
-	 * Count all the entities.
-	 * 
-	 * @since 1.0
-	 * @return the amount of entities.
-	 */
-	@Override
-	public Long countAll() {
-		return getDAO().countAll();
-	}
+  /**
+   * Count all the entities.
+   * 
+   * @since 1.0
+   * @return the amount of entities.
+   */
+  @Override
+  public Long countAll() {
+    return getDAO().countAll();
+  }
 
-	/**
-	 * Brings an entity by its key.
-	 * 
-	 * @since 1.0
-	 * @param id
-	 *            Entity key.
-	 * @return Entity.
-	 */
-	@Override
-	public Bean findByKey(Key id) {
-		Bean retValue = null;
-		if (id != null) {
-			retValue = getDAO().findByKey(id);
-		}
-		return retValue;
-	}
+  /**
+   * Brings an entity by its key.
+   * 
+   * @since 1.0
+   * @param id
+   *          Entity key.
+   * @return Entity.
+   */
+  @Override
+  public Bean findByKey(Key id) {
+    Bean retValue = null;
+    if (id != null) {
+      retValue = getDAO().findByKey(id);
+    }
+    return retValue;
+  }
 
-	/**
-	 * This is a hook so can be used to customize an action before the save
-	 * operation is successfully.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Bean.
-	 */
-	protected void beforeSave(Bean bean) {
-	}
+  /**
+   * This is a hook so can be used to customize an action before the save
+   * operation is successfully.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Bean.
+   */
+  protected void beforeSave(Bean bean) {
+  }
 
-	/**
-	 * This is a hook so can be used to customize an action after the save
-	 * operation is successfully.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Bean.
-	 */
-	protected void afterSave(Bean bean) {
-	}
+  /**
+   * This is a hook so can be used to customize an action after the save
+   * operation is successfully.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Bean.
+   */
+  protected void afterSave(Bean bean) {
+  }
 
-	/**
-	 * This is a hook so can be used to customize an action before the remove
-	 * operation is successfully.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Bean.
-	 */
-	protected void beforeRemove(Bean bean) {
-	}
+  /**
+   * This is a hook so can be used to customize an action before the remove
+   * operation is successfully.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Bean.
+   */
+  protected void beforeRemove(Bean bean) {
+  }
 
-	/**
-	 * This is a hook so can be used to customize an action after the remove
-	 * operation is successfully.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Bean.
-	 */
-	protected void afterRemove(Bean bean) {
-	}
+  /**
+   * This is a hook so can be used to customize an action after the remove
+   * operation is successfully.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Bean.
+   */
+  protected void afterRemove(Bean bean) {
+  }
 
-	/**
-	 * Saves the desired entity. It means it will persist a new entity or merge
-	 * an existent entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            desired entity.
-	 * @return entity stored
-	 */
-	@Override
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public Bean save(Bean bean) {
-		return save(bean, false);
-	}
+  /**
+   * Saves the desired entity. It means it will persist a new entity or merge an
+   * existent entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          desired entity.
+   * @return entity stored
+   */
+  @Override
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public Bean save(Bean bean) {
+    return save(bean, false);
+  }
 
-	/**
-	 * Saves the desired entity. It means it will persist a new entity or merge
-	 * an existent entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            desired entity.
-	 * @param flush
-	 *            If <code>true</code> the method {@link EntityManager#flush()}
-	 *            will be called.
-	 * @return entity stored
-	 */
+  /**
+   * Saves the desired entity. It means it will persist a new entity or merge an
+   * existent entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          desired entity.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   * @return entity stored
+   */
 
-	@Override
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public Bean save(Bean bean, boolean flush) {
-		if (bean == null) {
-			throw new NullPointerException("The bean parameter is mandatory.");
-		}
-		beforeSave(bean);
-		bean = getDAO().save(bean, flush);
-		afterSave(bean);
-		return bean;
-	}
+  @Override
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public Bean save(Bean bean, boolean flush) {
+    if (bean == null) {
+      throw new NullPointerException("The bean parameter is mandatory.");
+    }
+    beforeSave(bean);
+    bean = getDAO().save(bean, flush);
+    afterSave(bean);
+    return bean;
+  }
 
-	/**
-	 * Removes a desired entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Desired entity.
-	 */
-	@Override
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public void remove(Bean bean) {
-		this.remove(bean, false);
-	}
+  /**
+   * Removes a desired entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Desired entity.
+   */
+  @Override
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public void remove(Bean bean) {
+    this.remove(bean, false);
+  }
 
-	/**
-	 * Removes a desired entity.
-	 * 
-	 * @since 1.0
-	 * @param bean
-	 *            Desired entity.
-	 * @param flush
-	 *            If <code>true</code> the method {@link EntityManager#flush()}
-	 *            will be called.
-	 */
-	@Override
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public void remove(Bean bean, boolean flush) {
-		if (bean == null) {
-			throw new NullPointerException("The bean parameter is mandatory.");
-		}
-		beforeRemove(bean);
-		getDAO().remove(bean, flush);
-		afterRemove(bean);
-	}
+  /**
+   * Removes a desired entity.
+   * 
+   * @since 1.0
+   * @param bean
+   *          Desired entity.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   */
+  @Override
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public void remove(Bean bean, boolean flush) {
+    if (bean == null) {
+      throw new NullPointerException("The bean parameter is mandatory.");
+    }
+    beforeRemove(bean);
+    getDAO().remove(bean, flush);
+    afterRemove(bean);
+  }
+
+  /**
+   * Removes a desired entity.
+   * 
+   * @since 1.0
+   * @param key
+   *          Entity key.
+   */
+  @Override
+  public void removeById(Key key) {
+    removeById(key, false);
+  }
+
+  /**
+   * Removes a desired entity by an id.
+   * 
+   * @since 1.0
+   * @param key
+   *          Entity key.
+   * @param flush
+   *          If <code>true</code> the method {@link EntityManager#flush()} will
+   *          be called.
+   */
+  @Override
+  public void removeById(Key key, boolean flush) {
+    Bean entity = findByKey(key);
+    remove(entity, flush);
+  }
 }
